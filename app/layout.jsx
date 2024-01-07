601485b09471fbc3e6a16a1e810f160d163f20f3
duongni <133175915+duongni@users.noreply.github.com>
2024-01-06 23:24:13 -0500

    create a prompt app using Next.JS. Work on Navigation bar

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Prompt",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html Lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
