16639deb65769e95cea5a3874873a189d93337dd
duongni <133175915+duongni@users.noreply.github.com>
2024-01-09 13:51:14 -0500

    create form and route to create form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Form from "@components/Form";
+
 const CreatePrompt = () => {
-  return <div>Create Prompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    promt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,5 +1,28 @@
-const Form = () => {
-  return <div>Form</div>;
+import Link from "next/link";
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing with th world, and let your imagination run
+        wild with an AI-powered platform.
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea value={post.promt}></textarea>
+        </label>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
