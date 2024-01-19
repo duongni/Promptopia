783fbd7c05b2eeb7dd972a241dbb2e3b9dea1b45
duongni <133175915+duongni@users.noreply.github.com>
2024-01-19 12:39:44 -0500

    remove creator email from the site

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -45,9 +45,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
-            <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
-            </p>
           </div>
         </div>
 
